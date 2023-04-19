f74bfcecb23cb8db04677e2036c7078144e0cf13
Milena <milenasoaresf2@gmail.com>
2023-04-19 20:01:15 -0300

    adicionando metodos

--- GerenciarLocacao.java
+++ GerenciarLocacao.java
@@ -1,3 +1,27 @@
 public class GerenciarLocacao implements IGerenciar {
+
+    @Override
+    public void adicionar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionar'");
+    }
+
+    @Override
+    public void excluir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+    }
+
+    @Override
+    public void editarDados() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'editarDados'");
+    }
+
+    @Override
+    public void listar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
     
 }
