38a0827b179e5e05c4cff4e4a422ff3a5315ae43
sabrina <sabrinarfernandes.srf@gmail.com>
2023-04-19 21:03:38 -0300

    Classe categoria (atributos e métodos)

--- Categoria.java
+++ Categoria.java
@@ -1,3 +1,30 @@
 public class Categoria {
-    
+    private String nome;
+    private int identificador;
+
+    Categoria(int identificador, String nome){
+        this.setNome(nome);
+        this.setIdentificador(identificador);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    public void setIdentificador(int identificador){
+        this.identificador=identificador;
+    }
+
+    public String toString(){
+        return "Nome: "+getNome()+ " Identificador: "+getIdentificador();
+    }
+
 }
