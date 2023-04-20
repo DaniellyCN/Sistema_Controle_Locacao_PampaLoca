5ea2772fcbd4f03e0a42035bcdbc764ec5e2e6a5
DaniellyCN <89779359+DaniellyCN@users.noreply.github.com>
2023-04-19 22:33:20 -0300

    getters e setters de cliente

--- Cliente.java
+++ Cliente.java
@@ -10,6 +10,20 @@ public class Cliente{
         this.telefone = telefone;
         this.CPF = CPF;
      }
+    public String getCNH() { return CNH;}
 
+    public int getCPF() { return CPF;}
+
+    public String getNome() { return nome;}
+
+    public int getTelefone() { return telefone;}
+
+    public void setCNH(String cNH) { CNH = cNH;}
+
+    public void setCPF(int cPF) { CPF = cPF;}
+
+    public void setNome(String nome) { this.nome = nome;}
+
+    public void setTelefone(int telefone) { this.telefone = telefone;}
      
 }
